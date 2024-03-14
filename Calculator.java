dace33c8a73193cf0449a500e69b2f1693ebc4dd
Girivasan <girivasan203@gmail.com>
2024-03-14 11:47:56 +0530

    giri modified

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,23 @@
 class Calculator
   {
+	    public void add1(){
+                    int a=10;
+                    int b=20;
+            System.out.println(add);
+
+	     public void add2:wq
+		     (){
+                    int a=10;
+                    int b=20;
+            System.out.println(add);
+
+
+
     public static void main(String args[])
+	 
     {
+	    add1();
+	    add2();
+	   
     }
   }
