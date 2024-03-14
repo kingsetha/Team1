2bb35af92cc3fa82844a06939bcd86ba5cae5247
Ganapathy Raman <ganapathyramans@gmail.com>
2024-03-14 11:49:22 +0530

    ganapathy commit

--- Calculator.java
+++ Calculator.java
@@ -1,6 +1,22 @@
-class Calculator
-  {
+
+ public static void Mul1(){
+                    int a = 10;
+                    int b = 10;
+                    int c = a*b;
+                    System.out.println("Mul" ,+c);
+    }
+ public static void Mul2(){
+                    int a = 1;
+                    int b = 10;
+                    int c = a*b;
+                    System.out.println("Mul" ,+c);
+    }
+
+class Calculator {
+
     public static void main(String args[])
     {
+	    Mul1();
+	    Mul2();
     }
   }
